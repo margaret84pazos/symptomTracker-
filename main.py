4a10b507011aae3a3317b0b0f5cc08883e149b58
Margaret Pazos-Zarza <margaret84pazos@gmail.com>
2017-07-24 16:33:27 -0700

    Changes

--- main.py
+++ main.py
@@ -2,19 +2,65 @@ import webapp2
 import jinja2
 import os
 
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
-
-
 from google.appengine.api import users
+#appengine
 from google.appengine.ext import ndb
 
+class Profile(ndb.Model):
+    name = ndb.StringProperty()
+    sex = ndb.StringProperty()
+    age = ndb.StringProperty()
+    weight = ndb.StringProperty()
+    symptoms = ndb.StringProperty()
+
+class Symptoms(ndb.Model):
+    nameSymp = ndb.StringProperty()
+    time = ndb.DateTimeProperty(auto_now_add = True)
+    severity = ndb.IntegerProperty()
+
+jinja_environment = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        current_user = users.get_current_user()
+        logout_url = users.create_logout_url('/')
+        login_url = users.create_login_url('/')
+
+        template_vars = {
+            'current_user': current_user,
+            'logout_url': logout_url,
+            'login_url': login_url
+        }
+
+        template = jinja_environment.get_template("templates/home.html")
+        self.response.write(template.render(template_vars))
+
+
+class ProfileHandler(webapp2.RequestHandler):
+    def get(self):
+        profile_query = Profile.query()
+        profileInfo = profile_query.fetch()
+        template_vars = {
+            'profileInfo': profileInfo
+        }
+
         template = jinja_environment.get_template("templates/profile.html")
-        self.response.write(template.render(template_vars)
+        self.response.write(template.render(template_vars))
+
+    #def post(self):
+        #1. Get the information submitted in the form.
+        #name = self.request.get("name")
+        #sex = self.request.get("sex")
+        #age = self.request.get("age")
+        #weight = self.request.get("weight")
+        #symptoms = self.request.get("symptoms")
+
+        #profile = Profile(name =  name, sex = sex, age = age, weight = weight, #symptoms = symptoms)
+
+        #profile.put()
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/profile', ProfileHandler)
 ], debug=True)
